f67ce4de648de98cae819d7ab964ca74f1341dd0
Jonny Buchanan <jonathan.buchanan@gmail.com>
2019-07-28 23:01:38 +1000

    Initial commit

---
+++ README.md
@@ -0,0 +1,19 @@
+# Rllmuk Really Ignore Users
+
+![Rllmuk Logo](icons/icon128.png)
+
+* [Install Chrome Extension](https://chrome.google.com/webstore/detail/hlikbhdpmjloenbnbiibdljpgnmhdnnk)
+* [Install Firefox Add-on](https://addons.mozilla.org/en-US/firefox/addon/rllmuk-really-ignore-users/)
+* [Install as a user script](https://greasyfork.org/en/scripts/378211-rllmukzen-threadter) (requires a [user script manager](https://greasyfork.org/en#home-step-1))
+
+## After installing
+
+Visit your [Ignored Users page](https://www.rllmukforum.com/index.php?/ignore/) to synchronise your ignored users settings with the extension.
+
+The following will now be hidden in topics:
+
+- "You've chosen to ignore content by…" placeholders for posts by ignored users
+- Posts which quote an ignored user
+- Posts which @-mention an ignored user
+
+Please remind your fellow forumites to quote or @-mention ignore-worthy users when engaging with them.
---
+++ contentScript.js
@@ -0,0 +1,115 @@
+function addStyle(css) {
+  let $style = document.createElement('style')
+  $style.appendChild(document.createTextNode(css))
+  document.querySelector('head').appendChild($style)
+}
+
+function TopicPage() {
+  let ignoredUserIds = JSON.parse(localStorage.ignoredUserIds || '[]')
+
+  // Hide "You've chosen to ignore content by <ignored user>"
+  addStyle(`
+    .ipsComment_ignored {
+      display: none;
+    }
+  `)
+
+  // Hide posts containing elements which have an ignored user id as a specified
+  // data attribute.
+  function hidePostsByDataAttribute(elements, dataAttribute) {
+    elements.forEach(el => {
+      if (!ignoredUserIds.includes(el.dataset[dataAttribute])) return
+      let post = el.closest('article.ipsComment')
+      if (post.style.display == 'none') return
+      post.style.display = 'none'
+    })
+  }
+
+  // Hide posts which quote ignored users
+  function processQuotes(context) {
+    hidePostsByDataAttribute(
+      context.querySelectorAll('[data-ipsquote-userid]'),
+      'ipsquoteUserid'
+    )
+  }
+
+  // Hide posts which @-mention ignored users
+  function processMentions(context) {
+    hidePostsByDataAttribute(
+      context.querySelectorAll('[data-mentionid]'),
+      'mentionid'
+    )
+  }
+
+  // Hide the unread comment separator if all subseqent posts are hidden
+  function updateUnreadCommentSeparator() {
+    let separator = document.querySelector('hr.ipsCommentUnreadSeperator')
+    if (!separator) return
+    let hasVisiblePost = false
+    let sibling = separator.nextElementSibling
+    while (sibling) {
+      if (sibling.matches('article.ipsComment') &&
+          !sibling.classList.contains('ipsHide') &&
+          sibling.style.display != 'none') {
+        hasVisiblePost = true
+        break
+      }
+      sibling = sibling.nextElementSibling
+    }
+    separator.style.display = hasVisiblePost ? '' : 'none'
+  }
+
+  // Process all posts on the current page
+  function processPosts(context = document) {
+    processQuotes(context)
+    processMentions(context)
+  }
+
+  // Process initial posts
+  processPosts()
+  updateUnreadCommentSeparator()
+
+  // Watch for posts being replaced when paging
+  new MutationObserver(mutations =>
+    mutations.forEach(mutation => {
+      if (mutation.oldValue == 'true') {
+        processPosts()
+        updateUnreadCommentSeparator()
+      }
+    })
+  ).observe(document.querySelector('div.cTopic'), {
+    attributes: true,
+    attributeFilter: ['animating'],
+    attributeOldValue: true,
+  })
+
+  // Watch for new posts being loaded into the current page
+  new MutationObserver(mutations => {
+    mutations.forEach(mutation =>
+      mutation.addedNodes.forEach(processPosts)
+    )
+    updateUnreadCommentSeparator()
+  }).observe(document.querySelector('#elPostFeed > form'), {
+    childList: true,
+  })
+}
+
+function IgnoredUsersPage() {
+  // Sync ignored user ids
+  localStorage.ignoredUserIds = JSON.stringify(
+    Array.from(document.querySelectorAll('[data-ignoreuserid]')).map(el =>
+      el.dataset.ignoreuserid
+    )
+  )
+}
+
+let page
+if (location.href.includes('index.php?/topic/')) {
+  page = TopicPage
+} else if (location.href.includes('index.php?/ignore/')) {
+  page = IgnoredUsersPage
+}
+
+if (page) {
+  page()
+}
