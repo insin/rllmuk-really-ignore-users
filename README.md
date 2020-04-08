44f4de81797604b5f80b8f4b8a622a805ec92ff1
Jonny Buchanan <jonathan.buchanan@gmail.com>
2020-04-09 00:08:29 +1000

    Add ignoring users in specific topics and a new section in Ignored Users to manage them

--- README.md
+++ README.md
@@ -4,16 +4,30 @@
 
 * [Install Chrome Extension](https://chrome.google.com/webstore/detail/hlikbhdpmjloenbnbiibdljpgnmhdnnk)
 * [Install Firefox Add-on](https://addons.mozilla.org/en-US/firefox/addon/rllmuk-really-ignore-users/)
-* [Install as a user script](https://greasyfork.org/en/scripts/378211-rllmukzen-threadter) (requires a [user script manager](https://greasyfork.org/en#home-step-1))
+* [Install as a user script](https://greasyfork.org/en/scripts/378211) (requires a [user script manager](https://greasyfork.org/en#home-step-1))
 
 ## After installing
 
-Visit your [Ignored Users page](https://www.rllmukforum.com/index.php?/ignore/) to synchronise your ignored users settings with the extension.
+### Globally ignored users
+
+Visit the forum's [Ignored Users page](https://www.rllmukforum.com/index.php?/ignore/) and the extension will synchronise itself with your global ignored users settings.
 
 The following will now be hidden in topics:
 
 - "You've chosen to ignore content by…" placeholders for posts by ignored users
 - Posts which quote an ignored user
 - Posts which @-mention an ignored user
+
+### Ignoring users in specific topics
+
+The extension adds a new "Topic Ignore" control to user profile cards - hover over a user's name and click it to ignore them in the current topic only.
+
+![Example of the new Topic Ignore control being displayed](screenshots/topic_specific_ignore.png)
+
+The extension also adds a new section to the [Ignored Users page](https://www.rllmukforum.com/index.php?/ignore/) to manage users ignored in specific topics - click "Stop ignoring" here to stop ignoring a user in that topic.
+
+![Example of the new Topic Ignore control being displayed](screenshots/ignored_users.png)
+
+---
 
 Please remind your fellow forumites to quote or @-mention ignore-worthy users when engaging with them.
--- contentScript.js
+++ contentScript.js
@@ -1,3 +1,10 @@
+// ==UserScript==
+// @name        Rllmuk Really Ignore Users
+// @description Really ignore ignored users, and ignore users in specific topics
+// @namespace   https://github.com/insin/greasemonkey/
+// @version     6
+// @match       https://www.rllmukforum.com/index.php*
+// ==/UserScript==
 function addStyle(css) {
   let $style = document.createElement('style')
   $style.appendChild(document.createTextNode(css))
@@ -5,7 +12,14 @@ function addStyle(css) {
 }
 
 function TopicPage() {
+  let topicId = document.body.dataset.pageid
   let ignoredUserIds = JSON.parse(localStorage.ignoredUserIds || '[]')
+  let ignoredUsersInTopics = JSON.parse(localStorage.ignoredUsersInTopics || '{}')
+  let topicIgnoredUserIds = []
+  if (ignoredUsersInTopics[topicId]) {
+    topicIgnoredUserIds = ignoredUsersInTopics[topicId].users.map(user => user.id)
+    ignoredUserIds.push(...topicIgnoredUserIds)
+  }
 
   // Hide "You've chosen to ignore content by <ignored user>"
   addStyle(`
@@ -41,7 +55,21 @@ function TopicPage() {
     )
   }
 
-  // Hide the unread comment separator if all subseqent posts are hidden
+  // Hide posts by users ignored in this specific topic
+  function processTopicIgnoredPosts(context = document) {
+    if (topicIgnoredUserIds.length == 0) return
+
+    let postAvatarLinks = context.querySelectorAll('li.cAuthorPane_photo a')
+    postAvatarLinks.forEach(el => {
+      let userId = /profile\/(\d+)/.exec(el.href)[1]
+      if (!topicIgnoredUserIds.includes(userId)) return
+      let post = el.closest('article.ipsComment')
+      if (post.style.display == 'none') return
+      post.style.display = 'none'
+    })
+  }
+
+  // Hide the unread comment separator if all subsequent posts are hidden
   function updateUnreadCommentSeparator() {
     let separator = document.querySelector('hr.ipsCommentUnreadSeperator')
     if (!separator) return
@@ -63,11 +91,59 @@ function TopicPage() {
   function processPosts(context = document) {
     processQuotes(context)
     processMentions(context)
+    processTopicIgnoredPosts(context)
   }
 
   // Process initial posts
   processPosts()
   updateUnreadCommentSeparator()
+
+  // Add a new control to a user's hover card to ignore them in this topic
+  function processHoverCard($el) {
+    if (!$el.classList.contains('ipsHovercard')) return
+
+    // Create a new "Topic Ignore" control
+    let $topicIgnoreItem = document.createElement('li')
+    $topicIgnoreItem.innerHTML = `<a href="#">
+      <i class="fa fa-times-circle"></i> Topic Ignore
+    </a>`
+    let $ignoreLink = $topicIgnoreItem.querySelector('a')
+    $ignoreLink.addEventListener('click', (e) => {
+      e.preventDefault()
+
+      let topicName = document.querySelector('.ipsType_pageTitle').innerText
+      let user = {
+        id: /profile\/(\d+)/.exec($el.querySelector('a').href)[1],
+        name: $el.querySelector('h2').innerText,
+        avatar: $el.querySelector('img.ipsUserPhoto').src,
+      }
+
+      // Add the user to the ignored users config for this topic
+      let ignoredUsersInTopics = JSON.parse(localStorage.ignoredUsersInTopics || '{}')
+      if (ignoredUsersInTopics[topicId] == undefined) {
+        ignoredUsersInTopics[topicId] = {
+          name: topicName,
+          users: [],
+        }
+      }
+      ignoredUsersInTopics[topicId].name = topicName
+      ignoredUsersInTopics[topicId].users.push(user)
+      localStorage.ignoredUsersInTopics = JSON.stringify(ignoredUsersInTopics)
+
+      // Apply the new ignored user settings
+      ignoredUserIds.push(user.id)
+      topicIgnoredUserIds.push(user.id)
+      processPosts()
+      updateUnreadCommentSeparator()
+
+      // Hide the hover card
+      $el.style.display = 'none'
+    })
+
+    // Insert the new control into the hover card
+    let $findContentItem = $el.querySelector('ul.ipsList_inline li:last-child')
+    $findContentItem.parentNode.insertBefore($topicIgnoreItem, $findContentItem)
+  }
 
   // Watch for posts being replaced when paging
   new MutationObserver(mutations =>
@@ -92,6 +168,15 @@ function TopicPage() {
   }).observe(document.querySelector('#elPostFeed > form'), {
     childList: true,
   })
+
+  // Watch for user hover cards being added for display
+  new MutationObserver(mutations => {
+    mutations.forEach(mutation =>
+      mutation.addedNodes.forEach(processHoverCard)
+    )
+  }).observe(document.body, {
+    childList: true,
+  })
 }
 
 function IgnoredUsersPage() {
@@ -101,6 +186,78 @@ function IgnoredUsersPage() {
       el.dataset.ignoreuserid
     )
   )
+
+  // Add a new section to manage users ignored in specific topics
+  let $mainArea = document.querySelector('#ipsLayout_mainArea')
+  $mainArea.appendChild(document.createElement('br'))
+  let $div = document.createElement('div')
+  $div.className = 'ipsBox'
+
+  function populateIgnoredUsersInTopics() {
+    let ignoredUsersInTopics = JSON.parse(localStorage.ignoredUsersInTopics || '{}')
+
+    $div.innerHTML = `
+      <h2 class="ipsType_sectionTitle ipsType_reset ipsClear">Users currently being ignored in specific topics</h2>
+      <ol class="ipsDataList ipsGrid ipsGrid_collapsePhone ipsClear" data-role="tableRows">
+      </ol>`
+    let $ol = $div.querySelector('ol')
+
+    if (Object.keys(ignoredUsersInTopics).length == 0) {
+      $ol.innerHTML = `<li class="ipsDataItem">
+        <div class="ipsType_light ipsType_center ipsPad">
+          <br>
+          <br>
+          You're not currently ignoring any users in specific topics.
+        </div>
+      </li>`
+    }
+
+    for (let [topicId, topicConfig] of Object.entries(ignoredUsersInTopics)) {
+      for (let user of topicConfig.users) {
+        let $li = document.createElement('li')
+        $li.className = 'ipsDataItem ipsGrid_span6 ipsFaded_withHover'
+        $li.innerHTML = `
+          <p class="ipsType_reset ipsDataItem_icon">
+            <a href="https://www.rllmukforum.com/index.php?/profile/${user.id}" class="ipsUserPhoto ipsUserPhoto_tiny">
+              <img src="${user.avatar}" alt="${user.name}">
+            </a>
+          </p>
+          <div class="ipsDataItem_main">
+            <h4 class="ipsDataItem_title"><strong>${user.name}</strong></h4>
+            <ul class="ipsList_inline">
+              <li class="ipsType_light">
+                in <a href="https://www.rllmukforum.com/index.php?/topic/${topicId}">
+                  ${topicConfig.name}
+                </a>
+              </li>
+              <li>
+                <a href="#" class="unignore ipsPos_middle ipsType_blendLinks">
+                  <i class="fa fa-times-circle"></i> Stop ignoring
+                </a>
+              </li>
+            </ul>
+          </div>`
+        $li.querySelector('a.unignore').addEventListener('click', (e) => {
+          e.preventDefault()
+          let ignoredUsersInTopics = JSON.parse(localStorage.ignoredUsersInTopics || '{}')
+          if (!ignoredUsersInTopics[topicId]) return populateIgnoredUsersInTopics()
+          let index = ignoredUsersInTopics[topicId].users.findIndex(u => u.id == user.id)
+          if (index == -1) return populateIgnoredUsersInTopics()
+          ignoredUsersInTopics[topicId].users.splice(index, 1)
+          if (ignoredUsersInTopics[topicId].users.length == 0) {
+            delete ignoredUsersInTopics[topicId]
+          }
+          localStorage.ignoredUsersInTopics = JSON.stringify(ignoredUsersInTopics)
+          populateIgnoredUsersInTopics()
+        })
+        $ol.appendChild($li)
+      }
+    }
+
+    $mainArea.appendChild($div)
+  }
+
+   populateIgnoredUsersInTopics()
 }
 
 let page
